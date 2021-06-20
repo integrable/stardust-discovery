d55ebfd4005bb8849ded7b82bba1e2e8d881479d
Kamil Szewc <kamil.szewc@gmail.com>
2021-06-20 15:13:55 +0200

    Initial commit

---
+++ src/test/java/eu/integrable/numericalmechanicsdiscovery/NumericalMechanicsDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.integrable.numericalmechanicsdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumericalMechanicsDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
